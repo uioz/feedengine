097fd54cbf836356f732d4d98974d8d72e56b85c
randomguy <hzxbpld@163.com>
2022-11-19 16:33:48 +0800

    chore: drop the unnecessary package

--- packages/core/src/utils/log.ts
+++ packages/core/src/utils/log.ts
@@ -1,13 +1,12 @@
 import {pino, destination, type Logger} from 'pino';
-import type {SonicBoom} from 'sonic-boom';
 import {join} from 'node:path';
 import {env} from 'node:process';
 
 export const DEFAULT_LOG_NAME = 'feedengine.log';
 
-export type Log = Logger<SonicBoom>;
+export type Log = Logger;
 
-export function log({feedengine: {rootDir}}: {feedengine: {rootDir: string}}) {
+export function log({feedengine: {rootDir}}: {feedengine: {rootDir: string}}): Logger {
   return pino(
     env.NODE_ENV === 'production'
       ? destination({
